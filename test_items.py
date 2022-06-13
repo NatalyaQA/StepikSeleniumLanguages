4633fd9dec66ce436529e9a61811b9d4b03a2dc4
NatalyaQA <lna2007lna2007@gmail.com>
2022-06-13 20:12:34 +0500

    new test

---
+++ test_items.py
@@ -0,0 +1,24 @@
+# import pytest
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+import time
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+class TestProductPage:
+
+    def test_button_add_to_basket_is_visible(self, browser):
+        """Тест проверяет, что страница товара
+         содержит кнопку добавления в корзину
+        """
+        # Открываем страницу товара
+        browser.get(link)
+
+        # Установлено время принудительной задержки браузера
+        # после открытия страницы, для визуальной проверки языка открытой страницы
+        time.sleep(30)
+
+        # Проверяем наличие кнопки добавления товара в корзину
+        assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+
